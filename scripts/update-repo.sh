d017cced6e5c8692da145e4d8a2199f64090c3e4
arden <arden@tuta.io>
2024-05-17 14:51:18 -0500

    git some git going

---
+++ scripts/update-repo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# what? 
+# pulls an updated copy of this repo to the machine
+# why?
+# too lazy to do it in desktop mode
+
+dir=~/repos/arden/steamdeck
+
+cd dir
+git checkout main
+git pull
