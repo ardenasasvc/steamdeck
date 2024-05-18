79e02a445e0cfa3d4d78469f0e3c05e65d8877f4
arden <arden@tuta.io>
2024-05-18 15:41:57 -0500

    blop

--- scripts/stop-stardrop.sh
+++ scripts/stop-stardrop.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-
-# what? 
-# kills the stardrop internal process
-# why?
-# the process softlocks after playing in gaming mode, ie, cant exit the launcher without restarting the console
-
 pkill Internal
